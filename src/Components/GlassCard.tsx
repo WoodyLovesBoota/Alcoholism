99c0a3e8f9a75c30f27dc8b7d809c5b47cceacdd
woody <woodylovesboota@gmail.com>
2023-12-08 10:31:37 +0900

    add likestar component

--- src/Components/GlassCard.tsx
+++ src/Components/GlassCard.tsx
@@ -1,17 +1,20 @@
 import styled from "styled-components";
 import NavigationBar from "../Components/NavigationBar";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Categories from "../Components/Categories";
 import { useQuery } from "react-query";
 import { ICocktail, ICocktailDetail, getCocktailDetail, getCategoryResult, getAllCategoryResult } from "../api";
 import { useNavigate } from "react-router-dom";
 import { useRecoilState } from "recoil";
-import { cockTailState } from "../atoms";
+import { cockTailState, likesState } from "../atoms";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
 
 const GlassCard = ({ cocktail }: IGlassCardProps) => {
   const navigate = useNavigate();
   const [current, setCurrent] = useRecoilState(cockTailState);
-
+  const [isLike, setIsLike] = useRecoilState(likesState);
+  const [isIn, setIsIn] = useState(false);
   const { data, isLoading } = useQuery<ICocktailDetail>(["cocktails", cocktail.idDrink], () =>
     getCocktailDetail(cocktail.idDrink)
   );
@@ -21,12 +24,35 @@ const GlassCard = ({ cocktail }: IGlassCardProps) => {
     navigate(`/details/${data?.drinks[0].idDrink}`);
   };
 
+  const onStarClick = () => {
+    if (isIn) {
+      setIsLike((prev) => {
+        let index = -1;
+        for (let i = 0; i < isLike.length; i++) {
+          if (isLike[i].idDrink === cocktail.idDrink) index = i;
+        }
+        return [...prev.slice(0, index), ...prev.slice(index + 1)];
+      });
+    } else {
+      data && setIsLike((prev) => [...prev, data.drinks[0]]);
+    }
+  };
+
+  useEffect(() => {
+    for (let e of isLike) {
+      if (e.idDrink === cocktail.idDrink) setIsIn(true);
+    }
+  });
+
   return (
     <Wrapper>
       {!isLoading && (
         <Card onClick={onCardClick}>
           <Photo bgPhoto={data?.drinks[0].strDrinkThumb ? data?.drinks[0].strDrinkThumb : ""} />
           <Title>{cocktail.strDrink}</Title>
+          <Star isOn={isIn} onClick={onStarClick}>
+            <FontAwesomeIcon icon={faStar} />
+          </Star>
         </Card>
       )}
     </Wrapper>
@@ -73,6 +99,10 @@ const Title = styled.h2`
   display: flex;
   justify-content: center;
   text-align: center;
+`;
+
+const Star = styled.h2<{ isOn: boolean }>`
+  color: ${(props) => (props.isOn ? "yellow" : "black")};
 `;
 
 interface IGlassCardProps {
--- src/atoms.ts
+++ src/atoms.ts
@@ -1,5 +1,5 @@
 import { atom } from "recoil";
-import { ICocktailDetail } from "./api";
+import { ICocktailDetail, ICocktailSingle } from "./api";
 
 export const cockTailState = atom<ICocktailDetail>({
   key: "currentCocktail",
@@ -31,4 +31,44 @@ export const cockTailState = atom<ICocktailDetail>({
       },
     ],
   },
+});
+
+export const likesState = atom<ICocktailSingle[]>({
+  key: "likeCocktail",
+  default: [
+    {
+      idDrink: "",
+      strDrink: "",
+      strDrinkAlternate: "",
+      strCategory: "",
+      strAlcoholic: "",
+      strGlass: "",
+      strInstructions: "",
+      strDrinkThumb: "",
+      strIngredient1: "",
+      strIngredient2: "",
+      strIngredient3: "",
+      strIngredient4: "",
+      strIngredient5: "",
+
+      strMeasure1: "",
+      strMeasure2: "",
+      strMeasure3: "",
+      strMeasure4: "",
+      strMeasure5: "",
+
+      strImageSource: "",
+      strImageAttribution: "",
+    },
+  ],
+
+  effects: [
+    ({ setSelf, onSet }: any) => {
+      const savedValue = localStorage.getItem("likes");
+      if (savedValue !== null) setSelf(JSON.parse(savedValue));
+      onSet((newValue: any, _: any, isReset: boolean) => {
+        isReset ? localStorage.removeItem("likes") : localStorage.setItem("likes", JSON.stringify(newValue));
+      });
+    },
+  ],
 });
